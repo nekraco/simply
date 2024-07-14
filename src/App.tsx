c056550f060ed2dca8adb1ad9ef54ea7d4287287
nekraco <UrtaevAndreyA@gmail.com>
2024-07-14 16:50:52 +0300

    components in different files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import {Accordion} from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
   return (
@@ -11,49 +13,8 @@ function App() {
   );
 }
 
-function Rating() {
-  return (
-    <div>
-      <Star/>
-      <Star/>
-      <Star/>
-      <Star/>
-      <Star/>
-    </div>
-  );
-}
-
-function Accordion() {
-  return (
-    <div>
-      <AccordionTitle/>
-      <AccordionBody/>
-    </div>
-  );
-}
-
-function AccordionTitle() {
-  return <h3>Меню</h3>
-}
-
-function AccordionBody() {
-  return (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
-  )
-}
-
 function AppTitle() {
   return <>This is APP component</>
-}
-
-function Star() {
-  return (
-    <div>star</div>
-  )
 }
 
 export default App;
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+export function Accordion() {
+  return (
+    <div>
+      <AccordionTitle/>
+      <AccordionBody/>
+    </div>
+  );
+}
+
+function AccordionTitle() {
+  return <h3>Меню</h3>
+}
+
+function AccordionBody() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  )
+}
