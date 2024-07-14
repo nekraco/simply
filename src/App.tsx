feae485c231a1495526a0360a102f6f99a0bc514
nekraco <UrtaevAndreyA@gmail.com>
2024-07-14 16:30:45 +0300

    components in a single file

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,56 @@ import './App.css';
 function App() {
   return (
     <div>
-          This is APP component
+      <AppTitle/>
+      <Rating/>
+      <Accordion/>
     </div>
   );
+}
+
+function Rating() {
+  return (
+    <div>
+      <Star/>
+      <Star/>
+      <Star/>
+      <Star/>
+      <Star/>
+    </div>
+  );
+}
+
+function Accordion() {
+  return (
+    <div>
+      <AccordionTitle/>
+      <AccordionBody/>
+    </div>
+  );
+}
+
+function AccordionTitle() {
+  return <h3>Меню</h3>
+}
+
+function AccordionBody() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  )
+}
+
+function AppTitle() {
+  return <>This is APP component</>
+}
+
+function Star() {
+  return (
+    <div>star</div>
+  )
 }
 
 export default App;
